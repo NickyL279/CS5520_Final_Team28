ceb76ce78933c2f8899cc203f1f495d769151604
Jiangwei-shi <56906163+Jiangwei-shi@users.noreply.github.com>
2022-04-29 00:44:57 -0400

    add coin system

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+	public float turnSpeed = 90f;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		//check the object we collided with is the player
+		if(other.gameObject.name != "Player"){
+			return;
+		}
+		//Add to the player's score
+		GameManager.inst.IncrementScore();
+		//Destory this coin
+		Destroy(gameObject);
+	}
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0,0,turnSpeed * Time.deltaTime);
+    }
+}
